1e474ac4dabb6f9232bcd569480fcad596491c73
zfkun <zfkun@msn.com>
2014-03-25 11:50:47 +0800

    init todo-list demo for rin

---
+++ demo/todo/edp-build-config.js
@@ -0,0 +1,71 @@
+exports.input = __dirname;
+
+var path = require( 'path' );
+exports.output = path.resolve( __dirname, 'output' );
+
+var moduleEntries = 'html,htm,phtml,tpl,vm,js';
+var pageEntries = 'html,htm,phtml,tpl,vm';
+
+exports.getProcessors = function () {
+    return [ 
+        new LessCompiler( {
+            exclude: [],
+            entryExtnames: pageEntries,
+            compileOptions: {
+                compress: false
+            }
+        } ), 
+        new CssCompressor(),
+        new ModuleCompiler( {
+            exclude: [],
+            configFile: 'module.conf',
+            entryExtnames: moduleEntries
+        } ), 
+        new JsCompressor(), 
+        new PathMapper( {
+            replacements: [
+                { type: 'html', tag: 'link', attribute: 'href', extnames: pageEntries },
+                { type: 'html', tag: 'img', attribute: 'src', extnames: pageEntries },
+                { type: 'html', tag: 'script', attribute: 'src', extnames: pageEntries },
+                { extnames: moduleEntries, replacer: 'module-config' },
+                { extnames: 'css,less', replacer: 'css' }
+            ],
+            from: 'src',
+            to: 'asset'
+        } ) 
+    ];
+};
+
+exports.exclude = [
+    '/tool',
+    '/doc',
+    '/test',
+    '/module.conf',
+    '/dep/packages.manifest',
+    '/dep/*/*/test',
+    '/dep/*/*/doc',
+    '/dep/*/*/demo',
+    '/dep/*/*/tool',
+    '/dep/*/*/*.md',
+    '/dep/*/*/package.json',
+    '/edp-*',
+    '/.edpproj',
+    '.svn',
+    '.git',
+    '.gitignore',
+    '.idea',
+    '.project',
+    'Desktop.ini',
+    'Thumbs.db',
+    '.DS_Store',
+    '*.tmp',
+    '*.bak',
+    '*.swp'
+];
+
+exports.injectProcessor = function ( processors ) {
+    for ( var key in processors ) {
+        global[ key ] = processors[ key ];
+    }
+};
+
