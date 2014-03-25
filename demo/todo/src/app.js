5311013b42104c7143eac879932d41d5672be51f
zfkun <zfkun@msn.com>
2014-03-26 00:00:30 +0800

    update demo

---
+++ demo/todo/src/app.js
@@ -0,0 +1,21 @@
+/**
+ * Saber UI
+ * Copyright 2014 Baidu Inc. All rights reserved.
+ * 
+ * @file app
+ * @author zfkun(zfkun@msn.com)
+ */
+
+define(function (require) {
+
+    var ui = require( 'ui' );
+
+    return {
+
+        init: function () {
+            ui.init( '#todoapp' );
+        }
+
+    };
+
+});
---
+++ demo/todo/src/ui.js
@@ -0,0 +1,20 @@
+/**
+ * Saber UI
+ * Copyright 2013 Baidu Inc. All rights reserved.
+ * 
+ * @file ui
+ * @author zfkun(zfkun@msn.com)
+ */
+
+define( function ( require ) {
+
+    var ui = {};
+    var components = {};
+
+    ui.init = function ( wrap, options ) {
+        
+    };
+
+    return ui;
+
+});
